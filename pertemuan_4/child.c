4f1cfc249c65596d1adbc6c7d7286b8230595b41
muhffikkri <moehammad.fikr@gmail.com>
2025-11-26 09:54:54 +0700

    First commit

---
+++ pertemuan_4/child.c
@@ -0,0 +1,11 @@
+// Child Program
+#include <stdio.h>
+#include <unistd.h>
+int main(void)
+{
+while(1)
+{
+printf("Child");
+sleep(1);
+}
+}
---
+++ pertemuan_4/parent.c
@@ -0,0 +1,31 @@
+
+
+// Parent Program
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <error.h>
+#include <signal.h>
+#include <errno.h>
+int main(void)
+{
+pid_t pid;
+int status;
+if((pid =fork()) < 0)
+{
+// probably out of processes
+status =-1;
+}
+else if (pid == 0)
+{
+// in child so we execute process
+// use the execl function to to run a shell an execute the child program
+execl("/bin/sh","sh","-c","./child",(char *)0);
+}
+while(1)
+{
+sleep(1);
+printf("Parent");
+}
+printf("end of program");
+}
